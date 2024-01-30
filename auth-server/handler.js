26166e76e64798666755e8f354e39aedcdb5cbd8
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-01-29 21:05:19 -0500

    removed else statement

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -93,9 +93,7 @@ module.exports.getCalendarEvents = async (event) => {
         if (error) {
           return reject(error);
         }
-        else {
-          return resolve(response);
-        }
+        return resolve(response);
       }
     );
   })
