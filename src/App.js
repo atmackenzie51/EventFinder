ced312c15226c85d65c0a0674832b795d4a4502d
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-02-07 21:29:57 -0500

    added NOE to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ const App = () => {
     <div className="App">
       <CitySearch />
       <EventList />
+      <NumberOfEvents />
     </div>
   );
 }
