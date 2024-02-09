30f079f4280b529abe47ea520cfd54c62ccb5bcc
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-02-09 17:42:58 -0500

    added integration testing for eventlist

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { getEvents } from './api';
 
 
 
 const App = () => {
+  const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    setEvents(allEvents.slice(0, currentNOE));
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <CitySearch />
-      <EventList />
+      <EventList events={events} />
       <NumberOfEvents />
     </div>
   );
--- src/__test__/EventList.test.js
+++ src/__test__/EventList.test.js
@@ -1,5 +1,6 @@
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import EventList from '../components/EventList';
+import App from '../App';
 
 
 describe('<EventList /> component', () => {
@@ -19,3 +20,17 @@ describe('<EventList /> component', () => {
     expect(EventListComponent.getAllByRole("listitem")).toHaveLength(4);
   });
 });
+
+describe('<EventList /> integration', () => {
+  test('renders a list of 32 events when the app is mounted and rendered', async () => {
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(32);
+    });
+  });
+
+
+})
