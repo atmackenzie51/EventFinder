1744f3a727f099c2f922e5e0fe67f95009947db5
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-03-17 21:24:44 -0400

    adding warningalert

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -48,6 +48,14 @@ class ErrorAlert extends Alert {
   }
 }
 
+class WarningAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'rgb(255, 255, 0)'; //yellow
+    this.bgColor = 'rgb(255, 220, 200)'; //light yellow
+  }
+}
+
 export {
   InfoAlert, ErrorAlert
 };
