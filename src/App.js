b939ff59e494af51c462a0b67aa9284fb63e4bf1
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-03-17 21:42:37 -0400

    added the warning check to the application

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 import { useState, useEffect } from 'react';
 import { getEvents, extractLocations } from './api';
-import { InfoAlert, ErrorAlert } from './components/Alert'
+import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert'
 
 
 
@@ -16,8 +16,14 @@ const App = () => {
   const [currentCity, setCurrentCity] = useState("See all cities");
   const [infoAlert, setInfoAlert] = useState('');
   const [errorAlert, setErrorAlert] = useState('');
+  const [warningAlert, setWarningAlert] = useState('');
 
   useEffect(() => {
+    if (navigator.onLine) {
+      setWarningAlert('');
+    } else {
+      setWarningAlert('You are offline, events are loaded from a cache.');
+    }
     fetchData();
   }, [currentCity, currentNOE]);
 
@@ -37,6 +43,7 @@ const App = () => {
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={infoAlert} /> : null}
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null}
+        {warningAlert.length ? <WarningAlert text={warningAlert} /> : null}
       </div>
       <CitySearch
         allLocations={allLocations}
