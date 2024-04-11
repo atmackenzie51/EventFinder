39fb5762c76c8bf587396c2127046aacb9ad8a50
atmackenzie51 <126356439+atmackenzie51@users.noreply.github.com>
2024-04-11 19:03:53 -0400

    moving percentage sign off of label

--- src/components/EventGenresChart.js
+++ src/components/EventGenresChart.js
@@ -47,7 +47,7 @@ const EventGenresChart = ({ events }) => {
     index,
   }) => {
     const RADIAN = Math.PI / 180;
-    const radius = outerRadius;
+    const radius = outerRadius + 10;
     const x = cx + radius * Math.cos(-midAngle * RADIAN) * 1.07;
     const y = cy + radius * Math.sin(-midAngle * RADIAN) * 1.07;
     return percent ? (
